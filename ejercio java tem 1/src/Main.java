44729350b8718a520ee23e021c6dec212d050d16
alexander6591 <37714949+alexander6591@users.noreply.github.com>
2023-04-06 08:42:15 -0400

    Add files via upload

---
+++ ejercio java tem 1/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        int numero;
+        long numeroGrande;
+        double decimal;
+        boolean binario;
+        String texto;
+
+        numero=12345678;
+        numeroGrande=1234567890;
+        decimal=1.44;
+        binario=true;
+        texto="hola mundo";
+
+        System.out.println(numero);
+        System.out.println(numeroGrande);
+        System.out.println(decimal);
+        System.out.println(binario);
+        binario=false;
+        System.out.println(binario);
+        System.out.println(texto);
+
+    }
+}
